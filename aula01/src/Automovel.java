7b64bbf022b257f84d6bbb404900dd22606afcba
Geovana Rodrigues <159202999+geovanards@users.noreply.github.com>
2026-04-15 10:14:38 -0300

    Add files via upload

---
+++ aula01/src/Automovel.java
@@ -0,0 +1,14 @@
+public class Automovel {
+    String marca;
+    String modelo;
+    int ano;
+
+    public static void main(String[]args) {
+	
+		Automovel a1 = new Automovel();
+		a1.modelo ="GT3";
+		a1.marca = "Porsche";
+		a1.ano = 2017;
+        
+    }
+}
